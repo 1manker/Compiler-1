4c3c146a2587fa11a109961b803563692c0360e3
Lucas Manker <lmanker@uwyo.edu>
2019-09-15 11:51:20 -0600

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,6 @@
+##Makefile
+##Lucas Manker
+##9/12/19
+
+prog1: program1.cpp
+	g++ -ggdb -Wall -std=c++11 program1.cpp -o program1
---
+++ program1.cpp
@@ -0,0 +1,20 @@
+//program1.cpp
+//Lucas Manker
+//Cosc 4785 Fall 2019
+//Program 1
+//9/13/19
+
+#include <FlexLexer.h>
+
+int main(){
+
+    yyFlexLexer myScanner;
+    int rtn;
+
+    while((rtn = myScanner.yylex() != 0)){
+
+    }
+
+
+    return 0;
+}
